c79c94c9641be1bd4e1b0c0b64a0b1912049b0ab
Tayo Adekoya <Tayoadekoya96@gmail.com>
2022-11-03 16:19:56 -0600

    update

--- var.tf
+++ var.tf
@@ -1,3 +1,9 @@
-variable "rgname" {}
+variable "rgname" {
+ type        = string
+ description = "The name of resource group"
+}
 
-variable "location" {}
+variable "location" {
+ type        = string
+ description = "Location of resource group"
+}
